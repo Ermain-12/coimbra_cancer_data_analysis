42280620d37f948df3393cfe89aead7694cd4c55
Ermain <paulermain@gmail.com>
2018-09-08 21:43:30 -0400

    Preliminary analysis almost complete but I'm still working on implementing the Logistic Regression Analysis

--- src/main/scala/org/ermain/scala/spark/ciombra/data_processing/PreliminaryAnalysis.scala
+++ src/main/scala/org/ermain/scala/spark/ciombra/data_processing/PreliminaryAnalysis.scala
@@ -10,7 +10,6 @@ object PreliminaryAnalysis {
 
     println(dataInput.printSchema())
 
-
     // Split the data into a training and testing set
     val seed: Double = 98765L
     val dataSplit: Array[Dataset[Row]] = dataInput.randomSplit(Array(0.85, 0.15))
@@ -22,10 +21,10 @@ object PreliminaryAnalysis {
     val sqlDF = session.sql("SELECT Age, BMI FROM coimbra WHERE Age BETWEEN 20 AND 40")
     sqlDF.show(10)
 
-    val train = dataSplit(0)
-    // println(s"Training set size is ${trainingSet.count()}")
+    val trainingSet = dataSplit(0)
+    println(s"Training set size is ${trainingSet.count()}")
 
     val testingSet = dataSplit(1)
-    // println(s"Testing set size is ${testingSet.count()}")
+    println(s"Testing set size is ${testingSet.count()}")
   }
 }
